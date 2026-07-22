49ae709ff32a7299a33e463bd8dd9f6b27509303
Bhavya <mchandrashekhar173@gmail.com>
2026-07-22 19:38:51 +0530

    Fix static and media files

--- trustworthy_and_reliable_deep_learning/settings.py
+++ trustworthy_and_reliable_deep_learning/settings.py
@@ -114,14 +114,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Template/images')]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Template/images')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
-
-STATIC_ROOT = '/static/'
-
-STATIC_URL = '/static/'
